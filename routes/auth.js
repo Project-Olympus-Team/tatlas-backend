1892a0eccc0cf05ba1d18938cc6ca31b1ec621bf
Glory42 <gorkemkaryol03@gmail.com>
2025-03-18 22:23:47 +0300

    feat: Add Google OAuth authentication routes

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
+// Kullanıcı kayıt işlemi için route
 router.post('/register', authController.register);
+// Kullanıcı giriş işlemi için route
 router.post('/login', authController.login);
 
 module.exports = router;
