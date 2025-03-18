093305227a3ba29045f8c24479f9011a1b0d5bdb
Glory42 <gorkemkaryol03@gmail.com>
2025-03-18 22:26:10 +0300

    feat: Add test route and comments to server.js

--- server.js
+++ server.js
@@ -4,15 +4,22 @@ const pool = require('./config/db');
 const morgan = require('morgan');
 const cors = require('cors');
 
+// middleware eklemece
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(cors());
 
+// test route, ana sayfa
+app.get('/', (req, res) => {
+    console.log('Homepage');
+    res.send('Test 1234')
+});
 
+// auth işleme yönlendirmece
 const authRoutes = require('./routes/auth');
-
 app.use('/auth', authRoutes);
 
+// server bağlantısı
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running on ${port}`);
